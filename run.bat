2dde2ac0a40536f2f3c8f83aed47e8fa2c7e07ef
NIK2703 <44705381+NIK2703@users.noreply.github.com>
2019-04-05 15:09:48 +0700

    Update run.bat
    Исправление run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-javac -d bin -sourcepath src -encoding UTF-8 src\com\nikita_ogurnoy\lab_11\Main.java
-java -classpath bin com.nikita_ogurnoy.lab_11.Main
+javac -d bin -sourcepath src -encoding UTF-8 src\com\company\Main.java
+java -classpath bin com.company.Main
 pause
